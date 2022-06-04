839b9441937a9b192f242b5d75a7ee3bcec5e170
Daniele Marighetti Cox <40305033+danielecox@users.noreply.github.com>
2022-06-03 22:19:54 -0300

    Async Await

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
 const redux = require('redux')
-const prompts = require('prompts');
-const { text } = require('prompts/dist/prompts');
 
-const realizarVestibular = (nome, cpf) =>{
-    const extre6E10 = Math.random() <=7;
-    const nota = extre6E10 ? 6 + Math.random() *4:Math.random() *5
-    return{
+
+//criadora de acao pra realizar
+const realizarVestibular = (nome, cpf) => {
+    const entre6E10 = Math.random() <= 0.7;
+    const nota = entre6E10 ? 6 + Math.random() * 4 : Math.random() * 6;
+    // criar acao objeto json
+    return {
+        //tipo é requerido
         type: "REALIZAR_VESTIBULAR",
-        payload:{
-            nome, cpf,nota
+        //carga util pay load --- no doc não obrigatório
+        payload: {
+            nome, cpf, nota
         }
     }
 }
+//reducer acessar fatias de estado
 
-const realizarMatricula = (cpf, status)=>{
+
+//funcao criadora de acao
+const realizarMatricula = (cpf, status) => {
     return {
         type: "REALIZAR_MATRICULA",
-        payload:{
-            cpf,status
+        payload: {
+            cpf, status
         }
     }
 }
 //não há estados compartilhados em redux
-const historicoVestibular =(historicoVestibularAtual = [], acao)=>{
-    if(acao.type ==="REALIZAR_VESTIBULAR"){
-        return[
+const historicoVestibular = (historicoVestibularAtual = [], acao) => {
+    if (acao.type === "REALIZAR_VESTIBULAR") {
+        return [
             ...historicoVestibularAtual,
             acao.payload
         ]
@@ -33,38 +39,40 @@ const historicoVestibular =(historicoVestibularAtual = [], acao)=>{
 }
 
 
-const historicoMatricula= (historicoMatriculaAtial = [], acao){
-    if(acao.type ==="REALIZAR_MATRICULA"){
-        return[...historicoMatriculaAtial, acao.payload]
+const historicoMatricula = (historicoMatriculaAtial = [], acao) => {
+    if (acao.type === "REALIZAR_MATRICULA") {
+        return [
+            ...historicoMatriculaAtial,
+            acao.payload]
     }
     return historicoMatriculaAtial
 }
 const todosReducers = redux.combineReducers({
-    historicoVestibular, 
+    historicoVestibular,
     historicoMatricula
 })
 
 const store = redux.createStore(todosReducers)
 
 
-const main = async() =>{
+const main = async () => {
     const menu = '1-Realizar vestibular \n2-Realizar matricula\n 4-Visualizar a lista\n0- Sair'
-    let responsedo
-    do{
+    let response
+    do {
         Response = await prompts({
             type: 'number',
             name: 'op',
             message: menu
         })
-        switch(responsedo.op){
-            case 1:{
+        switch (response.op) {
+            case 1: {
 
-                const {nome} = await prompts({
+                const { nome } = await prompts({
                     type: text,
                     name: "nome",
                     message: 'Digite o nome'
                 })
-                const {cpf} = await prompts({
+                const { cpf } = await prompts({
                     type: text,
                     name: "cpf",
                     message: 'Digite o cpf'
@@ -73,48 +81,48 @@ const main = async() =>{
                 store.dispatch(acao)
                 break;
             }
-                case 2:  
+            case 2:
                 {
 
-                    const {cpf} = await prompts({
+                    const { cpf } = await prompts({
                         type: text,
                         name: "cpf",
                         message: 'Digite o cpf'
                     })
-                    const aprovado = store.getState().historicoVestibular.find((aluno)=>{
-                        return aluno.cpf === cpf && aluno >=6
+                    const aprovado = store.getState().historicoVestibular.find((aluno) => {
+                        return aluno.cpf === cpf && aluno >= 6
                     })
-                    if(aprovado){
+                    if (aprovado) {
                         const acao = realizarMatricula(cpf, 'M')
                         store.dispatch(acao)
                         console.log("Parabens matriculado")
                     }
-                    else{
+                    else {
                         const acao = realizarMatricula(cpf, 'NM')
                         store.dispatch(acao)
                         console.log("Não matriculado")
                     }
                     break;
                 }
-            case 3:{
-                const {cpf} = await prompts({
+            case 3: {
+                const { cpf } = await prompts({
                     type: text,
                     name: "cpf",
                     message: 'Digite o cpf'
                 })
-                const aluno = store.getState().historicoMatricula.find((a)=> a.cpf == cpf)
-                if(aluno){
+                const aluno = store.getState().historicoMatricula.find((a) => a.cpf == cpf)
+                if (aluno) {
                     console.log(aluno.status)
-                }else{
+                } else {
                     console.log("Aluno não existe")
                 }
 
             }
-            case 4:{
-                const aprovados = store.getState().historicoVestibular.filter(aluno => aluno.nota >=6)
+            case 4: {
+                const aprovados = store.getState().historicoVestibular.filter(aluno => aluno.nota >= 6)
                 console.log(aprovado)
             }
 
         }
-    }while(responsedo.op != 0)
+    } while (response.op != 0)
 }
